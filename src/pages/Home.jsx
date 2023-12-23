1eab1d6470f9627d2c132e5f468a0c3d68d918d0
Sonualam <sonualam1599@gmail.com>
2023-12-23 22:12:11 +0530

    added dynamic svg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,26 @@
 import ColumnCard from "../component/ColumnCard";
+import backlog from "../assets/backlog.svg";
+import cancel from "../assets/cancel.svg";
+import todo from "../assets/todo.svg";
+import inprogress from "../assets/inprogress.svg";
+import done from "../assets/done.svg";
 
 function Home() {
   return (
     <div className=" grid grid-cols-5 gap-[15px] w-[97%] m-auto h-[92vh] px-0 py-3  sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  ">
-      <ColumnCard />
+      <ColumnCard title="Backlog" icon={backlog} />
 
       {/* 22222222222222222 */}
 
-      <ColumnCard />
+      <ColumnCard title="Todo" icon={todo} />
       {/* 3333333333333333333333 */}
-      <ColumnCard />
+      <ColumnCard title="In progress" icon={inprogress} />
 
       {/* 444444444444444444444444444444? */}
-      <ColumnCard />
+      <ColumnCard title="Done" icon={done} />
 
       {/* 55555555555555555555555555555555555 */}
-      <ColumnCard />
+      <ColumnCard title="Cancelled" icon={cancel} />
     </div>
   );
 }
